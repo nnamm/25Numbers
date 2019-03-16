9ba85f4b01128ac441c9f2092090458c8160fc8a
花村貴史 <nnamm@MBPR2018.local>
2019-03-17 02:31:44 +0900

    初期化処理の整備などゲームらしく動作するよう対応。UI調整。

--- main.js
+++ main.js
@@ -1,144 +1,160 @@
-const gameNumbers = [
-    1, 2, 3, 4, 5,
-    6, 7, 8, 9, 10,
-    11, 12, 13, 14, 15,
-    16, 17, 18, 19, 20,
-    21, 22, 23, 24, 25
-];
-const bColor = "#fd9535"; // Button color
-const bColorSuccess = "#fff"; // Button color (Success)
-let btnColor = []; // Array of Button color data
-let checkCounter = 0; // Pushable button number
+const NUMBERS1 = ['01', '02', '03', '04', '05']; // Initial value of Button Row1
+const NUMBERS2 = ['06', '07', '08', '09', '10']; // Row2
+const NUMBERS3 = ['11', '12', '13', '14', '15']; // Row3
+const NUMBERS4 = ['16', '17', '18', '19', '20']; // Row4
+const NUMBERS5 = ['21', '22', '23', '24', '25']; // Row5
+const BUTTON_COLOR = '#fd9535'; // Button color
+const BUTTON_SUCCESS = '#fff'; // Button color (Success)
+const CLEAR_MESSAGE = "Congrats!" // Clear message
 
+// Game numbers 1-25
+let gameNumbers = () => NUMBERS1.concat(NUMBERS2).concat(NUMBERS3).concat(NUMBERS4).concat(NUMBERS5);
+
+// Vue
 let app = new Vue({
-    el: '#app',
+  el: '#app',
 
-    created: function () {
-        setButtonColor();
-    },
+  data: {
+    gameNumbers: gameNumbers(), // Game numbers 1-25
+    buttonRow1: NUMBERS1, // 5 numbers of Row1
+    buttonRow2: NUMBERS2, // Row2
+    buttonRow3: NUMBERS3, // Row3
+    buttonRow4: NUMBERS4, // Row4
+    buttonRow5: NUMBERS5, // Row5
+    colors: setButtonColor(), // 1-25 button color
 
-    data: {
-        buttons1: randomSort(gameNumbers), // 5 numbers of Row1
-        buttons2: randomSort(gameNumbers), // Row2
-        buttons3: randomSort(gameNumbers), // Row3
-        buttons4: randomSort(gameNumbers), // Row4
-        buttons5: randomSort(gameNumbers), // Row5
-        colors: btnColor, // 1-25 button color
-        startTime: 0, // Start button was pressed time
-        nowTime: 0, // Current time
-        diffTime: 0, // Difference time (nowTime - startTime)
-        animateFrame: 0, //
-        isRunning: false, // Start button flag
-        isStart: true // Game flag
-    },
+    checkCounter: 0, // Pushable button number
+    message: CLEAR_MESSAGE, // Finish message
 
-    methods: {
-        // Check clicked number
-        checkNumber(btnId) {
-            if (btnId == checkCounter) {
-                // Clear button color
-                this.$set(this.colors, btnId, bColorSuccess);
-                checkCounter++;
-                // Stop the timer when finished
-                if (checkCounter == 25) {
-                    this.stopTimer();
-                }
-            }
-        },
-        // 現在時刻から引数に渡した数値を startTime に代入
-        setSubtractStartTime(time) {
-            let tmpTime = typeof time !== 'undefined' ? time : 0;
-            this.startTime = Math.floor(performance.now() - tmpTime);
-        },
-        // Start the timer
-        startTimer() {
-            // loop()内で this の値が変更されるので退避
-            let vm = this;
-            vm.isStart = false;
-            vm.setSubtractStartTime(vm.diffTime);
-            // Loop process
-            (function loop() {
-                vm.nowTime = Math.floor(performance.now())
-                vm.diffTime = vm.nowTime - vm.startTime
-                vm.animateFrame = requestAnimationFrame(loop)
-            }());
-            vm.isRtunning = true
-        },
-        // Stop the timer
-        stopTimer() {
-            this.isRunning = false;
-            this.isStart = false;
-            cancelAnimationFrame(this.animateFrame);
-        },
-        // Todo: ボタンデータを初期化する
-        // Clear timer & button data
-        clearAllData() {
-            // Clear timer
-            this.startTime = 0;
-            this.nowTime = 0;
-            this.diffTime = 0;
-            this.stopTimer();
-            this.animateFrame = 0;
-            // Clear button data
-            setButtonColor();
-            checkCounter = 0;
+    startTime: 0, // Start button was pressed time
+    nowTime: 0, // Current time
+    diffTime: 0, // Difference time (nowTime - startTime)
+    animateFrame: 0, //
+
+    isStart: true, // Starting game flag
+    isRunning: false, // Start button visible flag
+    isShow: false //  
+  },
+
+  methods: {
+    // Change the button color when the button is clicked in order
+    checkNumber(clickBtnNum) {
+      if (clickBtnNum == this.checkCounter) {
+        // Clear button color
+        this.$set(this.colors, clickBtnNum, BUTTON_SUCCESS);
+        this.checkCounter++;
+        // Stop the timer when finished
+        if (this.checkCounter == 25) {
+          this.stopTimer();
+          this.isShow = true;
         }
+      }
     },
 
-    computed: {
-        // Calc hours
-        hours() {
-            return Math.floor(this.diffTime / 1000 / 60 / 60)
-        },
-        // Clac minites (60分になったら0分に戻る)
-        minutes() {
-            return Math.floor(this.diffTime / 1000 / 60) % 60
-        },
-        // Clas seconds (60秒になったら0秒に戻る)
-        seconds() {
-            return Math.floor(this.diffTime / 1000) % 60
-        },
-        // Calc mill sec (1000ミリ秒になったら0ミリ秒に戻る)
-        milliSeconds() {
-            return Math.floor(this.diffTime % 1000)
-        }
+    // Start the game
+    startGame() {
+      // Set the button randomly
+      this.buttonRow1 = randomSort(this.gameNumbers);
+      this.buttonRow2 = randomSort(this.gameNumbers);
+      this.buttonRow3 = randomSort(this.gameNumbers);
+      this.buttonRow4 = randomSort(this.gameNumbers);
+      this.buttonRow5 = randomSort(this.gameNumbers);
+
+      // Start the timer
+      let vm = this; // loop()内で this の値が変更されるので退避
+      vm.isStart = false;
+      vm.setSubtractStartTime(vm.diffTime);
+      // Loop process
+      (function loop() {
+        vm.nowTime = Math.floor(performance.now());
+        vm.diffTime = vm.nowTime - vm.startTime;
+        vm.animateFrame = requestAnimationFrame(loop);
+      })();
+      vm.isRunning = true;
     },
 
-    filters: {
-        // ゼロ埋めフィルタ 引数に桁数を入力する
-        zeroPad(value, num) {
-            let tmpNum = typeof num !== 'undefined' ? num : 2
-            return value.toString().padStart(tmpNum, "0")
-        }
+    // 現在時刻から引数に渡した数値を startTime に代入
+    setSubtractStartTime(time) {
+      let tmpTime = typeof time !== 'undefined' ? time : 0;
+      this.startTime = Math.floor(performance.now() - tmpTime);
+    },
+
+    // Stop the timer
+    stopTimer() {
+      this.isRunning = false;
+      this.isStart = false;
+      cancelAnimationFrame(this.animateFrame);
+    },
+
+    // Clear all game data
+    clearAllData() {
+      this.startTime = 0;
+      this.nowTime = 0;
+      this.diffTime = 0;
+      this.stopTimer();
+      this.animateFrame = 0;
+      this.gameNumbers = gameNumbers();
+      this.colors = setButtonColor();
+      this.checkCounter = 0;
+      this.isShow = false;
     }
+  },
+
+  computed: {
+    // Calc hours
+    hours() {
+      return Math.floor(this.diffTime / 1000 / 60 / 60);
+    },
+    // Clac minites (60分になったら0分に戻る)
+    minutes() {
+      return Math.floor(this.diffTime / 1000 / 60) % 60;
+    },
+    // Clas seconds (60秒になったら0秒に戻る)
+    seconds() {
+      return Math.floor(this.diffTime / 1000) % 60;
+    },
+    // Calc mill sec (1000ミリ秒になったら0ミリ秒に戻る)
+    milliSeconds() {
+      return Math.floor(this.diffTime % 1000);
+    }
+  },
+
+  filters: {
+    // ゼロ埋めフィルタ 引数に桁数を入力する
+    zeroPad(value, num) {
+      let tmpNum = typeof num !== 'undefined' ? num : 2;
+      return value.toString().padStart(tmpNum, '0');
+    }
+  }
 });
 
 /**
- * setButtonColor: ボタンカラーを初期カラーに設定する。
- * 
- * @return {void}
+ * randomSort: Set a number between 1 and 25 randomly.
+ *
+ * @param {Array of NUMBERS} num
+ *
+ * @return {Array}
  */
-function setButtonColor() {
-    [...Array(25)].forEach((_, i) => {
-        btnColor[i] = bColor;
-    })
+function randomSort(num) {
+  let arrayData = num;
+  let resultArray = [];
+  for (let i = 0; i < 5; i++) {
+    let arrayIndex = Math.floor(Math.random() * arrayData.length);
+    resultArray[i] = arrayData[arrayIndex];
+    arrayData.splice(arrayIndex, 1);
+  }
+  return resultArray;
 }
 
 /**
- * randomSort: 1〜25の数字を配列にランダム設定する。
- * 1行5列のボタンを5回に分けて作る想定。
- * 
- * @param {Array of gameNumbers} num
- *  
- * @return {Array} 
+ * setButtonColor: Set the button color to the initial color.
+ *
+ * @return {Array}
  */
-function randomSort(num) {
-    let arrayData = num;
-    let resultArray = [];
-    for (let i = 0; i < 5; i++) {
-        let arrayIndex = Math.floor(Math.random() * arrayData.length);
-        resultArray[i] = arrayData[arrayIndex];
-        arrayData.splice(arrayIndex, 1);
-    }
-    return resultArray;
+function setButtonColor() {
+  let btnColor = [];
+  [...Array(25)].forEach((_, i) => {
+    btnColor[i] = BUTTON_COLOR;
+  });
+  return btnColor;
 }
